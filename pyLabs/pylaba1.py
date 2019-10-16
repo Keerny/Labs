81c9e0e2c6d75e1e48917ecf88d04def4b89c8c0
Keerny <56045941+Keerny@users.noreply.github.com>
2019-10-16 13:45:24 +0300

    Create pylaba1.py

---
+++ pyLabs/pylaba1.py
@@ -0,0 +1,18 @@
+import math
+print ('Vvedite x')
+x=float(input())
+print ('Vvedite a')
+a=float(input())
+print ('G=',(4*(-18*a*a+3*a*x+10*x*x))/(15*a*a+29*a*x+12*x*x))
+
+print ('Vvedite x')
+x=float(input())
+print ('Vvedite a')
+a=float(input())
+print ('F=',math.tan(15*a*a-41*a*x+28*x*x))
+
+print ('Vvedite x')
+x=float(input())
+print ('Vvedite a')
+a=float(input())
+print ('Y=',math.acos(a*(-a)+3*a*x+4*x*x+1))
