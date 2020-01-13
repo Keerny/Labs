a8a23adaa7b34ff0df5492aa60dd8360d26c72b9
Keerny <56045941+Keerny@users.noreply.github.com>
2020-01-13 19:57:13 +0300

    Update pylaba8.py

--- pyLabs/pylaba8.py
+++ pyLabs/pylaba8.py
@@ -1,17 +1,19 @@
 import math
+import random
+import time
 
 c = 0
 n = int(input('Введите общее количество точек = '))
 r = float(input('Введите радиус = '))
-print('Введите координаты центра окружности')
-a = float(input())
-b = float(input())
+a = 0
+b = 0
+start_time = time.time()
 for i in range(1, n+1):
-    print('Введите координаты ',i,' точки')
-    x = float(input())
-    y = float(input())
+    x = random.uniform(0, 100)
+    y = random.uniform(0, 100)
     if math.sqrt(x-a)+math.sqrt(y-b)<=math.sqrt(r):
         c += 1
+    print(time.time() - start_time) 
 if c == 0:
     print('Ни одна точка не принадлежит окружности')
 else:
